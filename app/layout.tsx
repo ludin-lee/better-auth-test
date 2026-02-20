a0b39a88890eea70cc77ee1aa398c53a6024ee2d
ludin-lee <alsdyd0331@naver.com>
2026-02-20 14:36:49 +0900

    google tag_3

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,17 +25,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17943323588"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-              gtag('config', 'AW-17943323588');
-            `,
+  gtag('config', 'AW-17943323588');
+`,
           }}
         />
       </head>
